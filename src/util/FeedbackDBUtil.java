4242cc27f999645ea0e8db4ef0a7d225be25a08e
Kalana Wickramaarachchi <64887137+lonewol7f@users.noreply.github.com>
2021-03-20 12:12:33 +0530

    Update FeedbackDBUtil.java

--- src/util/FeedbackDBUtil.java
+++ src/util/FeedbackDBUtil.java
@@ -1,10 +1,13 @@
 package util;
 
+import model.Feedback;
+
 import java.sql.Connection;
 import java.sql.ResultSet;
 import java.sql.Statement;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
 
 public class FeedbackDBUtil {
 
@@ -34,5 +37,44 @@ public class FeedbackDBUtil {
         }
 
         return isSuccess;
+    }
+
+    public static ArrayList<Feedback> allFeedback(){
+        ArrayList<Feedback> feedbacks = new ArrayList<>();
+
+        try {
+            conn = DBConnectUtil.getConnection();
+            stmt = conn.createStatement();
+            String sql = "SELECT * FROM feedback ORDER BY feedbackID DESC";
+
+            rs = stmt.executeQuery(sql);
+
+            while (rs.next()){
+                Feedback feedback = new Feedback();
+                feedback.setFeedbackID(rs.getInt(1));
+                feedback.setEmail(rs.getString(2));
+                feedback.setType(rs.getString(3));
+                feedback.setComment(rs.getString(4));
+                feedback.setCreated(rs.getString(5));
+
+                feedbacks.add(feedback);
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }
+
+        return feedbacks;
+    }
+
+    public static void deleteFeedback(int id){
+        try {
+            conn = DBConnectUtil.getConnection();
+            stmt = conn.createStatement();
+
+            String sql = "DELETE FROM feedback WHERE feedbackID='"+id+"'";
+            stmt.executeUpdate(sql);
+        }catch (Exception e){
+            e.printStackTrace();
+        }
     }
 }
