6b83233b38fdaaa9a69e0b5fc5c760b90cfd4486
Kalana Wickramaarachchi <64887137+lonewol7f@users.noreply.github.com>
2021-03-20 21:35:03 +0530

    Update Feedback.java

--- src/model/Feedback.java
+++ src/model/Feedback.java
@@ -1,5 +1,8 @@
 package model;
 
+/**
+ * This is Feedback model class
+ */
 public class Feedback {
     private int feedbackID;
     private String email;
@@ -7,42 +10,82 @@ public class Feedback {
     private String comment;
     private String created;
 
+    /**
+     *
+     * @return feedbackID (int)
+     */
     public int getFeedbackID() {
         return feedbackID;
     }
 
+    /**
+     *
+     * @param feedbackID the feedbackID to set
+     */
     public void setFeedbackID(int feedbackID) {
         this.feedbackID = feedbackID;
     }
 
+    /**
+     *
+     * @return email (String)
+     */
     public String getEmail() {
         return email;
     }
 
+    /**
+     *
+     * @param email the email to set
+     */
     public void setEmail(String email) {
         this.email = email;
     }
 
+    /**
+     *
+     * @return type of feedback (String)
+     */
     public String getType() {
         return type;
     }
 
+    /**
+     *
+     * @param type the feedback type (Suggestions / Complain) to set
+     */
     public void setType(String type) {
         this.type = type;
     }
 
+    /**
+     *
+     * @return feedback comment (String)
+     */
     public String getComment() {
         return comment;
     }
 
+    /**
+     *
+     * @param comment the feedback comment to set
+     */
     public void setComment(String comment) {
         this.comment = comment;
     }
 
+    /**
+     *
+     * @return created date and time (String)
+     */
     public String getCreated() {
         return created;
     }
 
+    /**
+     *
+     * @param created feedback submitted date and time to set
+     */
     public void setCreated(String created) {
         this.created = created;
     }
