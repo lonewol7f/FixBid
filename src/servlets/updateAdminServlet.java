d4671f81385be7d45e01b6400128ae821c944c13
Kalana Wickramaarachchi <64887137+lonewol7f@users.noreply.github.com>
2021-04-12 21:54:32 +0530

    Create updateAdminServlet.java

---
+++ src/servlets/updateAdminServlet.java
@@ -0,0 +1,39 @@
+package servlets;
+
+import util.AdminDBUtil;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "updateAdminServlet")
+public class updateAdminServlet extends HttpServlet {
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String adminId = request.getParameter("adminId");
+        String firstName = request.getParameter("firstName");
+        String lastName = request.getParameter("lastName");
+        String phoneNo = request.getParameter("phoneNo");
+        String street = request.getParameter("street");
+        String city = request.getParameter("city");
+        String state = request.getParameter("state");
+        String zip = request.getParameter("zip");
+
+        int convertedAdminId = Integer.parseInt(adminId);
+        int convertedZip = Integer.parseInt(zip);
+
+        boolean success = AdminDBUtil.update(firstName, lastName, phoneNo, street, city, state, convertedZip, convertedAdminId);
+
+        if (success){
+            System.out.println("Update success"); // Testing only
+        }else {
+            System.out.println("Something went wrong!!!"); // Testing only
+        }
+    }
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+
+    }
+}
