6c6cd4853dcaefa85bc0b98f77f53a0200aac58f
Kalana Wickramaarachchi <64887137+lonewol7f@users.noreply.github.com>
2021-04-12 21:56:36 +0530

    Update admin.js

--- web/js/admin.js
+++ web/js/admin.js
@@ -2,16 +2,26 @@ function dashboard() {
     document.getElementById("dashboard-div").style.display = "block";
     document.getElementById("users-div").style.display = "none";
     document.getElementById("feedback-div").style.display = "none";
+    document.getElementById("profile-div").style.display = "none";
 }
 
 function users() {
     document.getElementById("dashboard-div").style.display = "none";
     document.getElementById("users-div").style.display = "block";
     document.getElementById("feedback-div").style.display = "none";
+    document.getElementById("profile-div").style.display = "none";
 }
 
 function feedBack() {
     document.getElementById("dashboard-div").style.display = "none";
     document.getElementById("users-div").style.display = "none";
     document.getElementById("feedback-div").style.display = "block";
+    document.getElementById("profile-div").style.display = "none";
+}
+
+function profile() {
+    document.getElementById("dashboard-div").style.display = "none";
+    document.getElementById("users-div").style.display = "none";
+    document.getElementById("feedback-div").style.display = "none";
+    document.getElementById("profile-div").style.display = "block";
 }
