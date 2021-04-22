11971621794a3bab5513e708d7ed62445b2b451e
RuwanaraT <sewminiruwanara@gmail.com>
2021-04-23 00:28:12 +0530

    Update user.java

--- src/model/User.java
+++ src/model/User.java
@@ -2,23 +2,22 @@ package model;
 
 public class User {
 
+    //private String UserAcc;
     private String FName;
     private String LName;
     private String address;
     private String phoneNumber;
     private String email;
     private String password;
-    private String RePassword;
 
-    public User(String FName, String LName, String address, String phoneNumber, String email, String password, String RePassword) {
-        this.FName = FName;
-        this.LName = LName;
-        this.address = address;
-        this.phoneNumber = phoneNumber;
-        this.email = email;
-        this.password = password;
-        this.RePassword = RePassword;
+
+    /*public String getUserAcc() {
+        return UserAcc;
     }
+
+    public void setUserAcc(String userAcc) {
+        UserAcc = userAcc;
+    }*/
 
     public String getFName() {
         return FName;
@@ -44,9 +43,7 @@ public class User {
         this.address = address;
     }
 
-    public String getPhoneNumber() {
-        return phoneNumber;
-    }
+    public String getPhoneNumber() { return phoneNumber; }
 
     public void setPhoneNumber(String phoneNumber) {
         this.phoneNumber = phoneNumber;
@@ -68,11 +65,5 @@ public class User {
         this.password = password;
     }
 
-    public String getRePassword() {
-        return RePassword;
-    }
 
-    public void setRePassword(String rePassword) {
-        RePassword = rePassword;
-    }
 }
