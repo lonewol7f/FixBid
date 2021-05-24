e1fd3684815207a36614fa00f0270600e2fb49f7
Sachintha Nirmal <sachintha919@gmail.com>
2021-05-24 20:34:29 +0530

    Update Product.java

--- src/model/Product.java
+++ src/model/Product.java
@@ -5,8 +5,10 @@ public class Product {
     private String pName;
     private String expDate;
     private int startPrice;
-    private int vendorID;
     private String description;
+    private String productType;
+    private int vendorID;
+
 
     //Product Constructor?????
 
@@ -16,9 +18,9 @@ public class Product {
     public void setPid(int pid) { this.pid = pid; }
 
     //Product Name
-    public String getPname() { return pName; }
+    public String getpName() { return pName; }
 
-    public void setPname(String pName) { this.pName = pName; }
+    public void setpName(String pName) { this.pName = pName; }
 
     //Bid expiry date
     public String getExpDate() { return expDate; }
@@ -29,4 +31,19 @@ public class Product {
     public int getStartPrice() { return startPrice; }
 
     public void setStartPrice(int startPrice) { this.startPrice = startPrice; }
+
+    //Description
+    public String getDescription() { return description; }
+
+    public void setDescription(String description) { this.description = description; }
+
+    //Product Type
+    public String getProductType() { return productType; }
+
+    public void setProductType(String productType) { this.productType = productType; }
+
+    //vendorID
+    public int getVendorID() { return vendorID; }
+
+    public void setVendorID(int vendorID) { this.vendorID = vendorID; }
 }
